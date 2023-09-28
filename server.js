ba8df460e772aa4b50b277b6d0f6574959650f66
manda06 <amanda.bass@eagles.oc.edu>
2023-09-28 16:36:58 -0500

    updated to auto-deploy

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ require("./app/routes/tutorial.routes")(app);
 require("./app/routes/lesson.routes")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 3100;
+const PORT = process.env.PORT || 3025;
 if (process.env.NODE_ENV !== "test") {
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
