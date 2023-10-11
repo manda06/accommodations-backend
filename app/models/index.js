65a321525986033616617326617d16266901deb4
sdrrfxc <kelby.criss@gmail.com>
2023-10-11 10:07:51 -0500

    Corrected error in index.js

--- app/models/index.js
+++ app/models/index.js
@@ -19,15 +19,14 @@ db.user = require("./user.model.js")(sequelize, Sequelize);
 db.session = require("./session.model.js")(sequelize, Sequelize);
 db.student = require("./student.model.js")(sequelize, Sequelize);
 db.request = require("./request.model.js")(sequelize, Sequelize);
-
 db.accommodation = require("./accommodation.model.js")(sequelize, Sequelize);
-db.student= require("./student.model.js")(sequelize, Sequelize);
-db.course= require("./course.model.js")(sequelize, Sequelize);
-
 
 // foreign keys for accommodation
-db.accommodation.belongsToMany(db.student, { through: 'studentAccomodation' });
-Accommodation.hasOne(db.course);
+db.accommodation.belongsToMany(
+  db.student, 
+  { through: 'studentAccomodation' });
+
+db.accommodation.hasOne(db.course);
 
 
 // foreign key for session
